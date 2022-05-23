8cda4153677bdc39994052e1c7522687988c7680
Chris Rowe <rowe2ry@yahoo.com>
2022-05-23 16:27:29 -0400

    added styling for the contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,17 +1,35 @@
-
-
 const Contact = () => {
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+    }
+
     return (
         <div className="background">
             <div className="container">
                 <div className="row">
-                    <article className="card">
+                    <article className="card contact__card">
                         <section className="card__header">
                             <h2 className="card__title">Contact Me</h2>
                         </section>
-                        <section className="card__body row">
-                            <div className="card__text">
-                                <p>Hello there! My name is Chris Rowe and I am a web developer with a love for problem solving and growth. I think its very important to strike the right blend of having fun and not taking one's self too seriosly, while still being productive and accomplishing tasks with precision and excellence. Life is about experiences, and creativity allows me to enhance the experience of others.</p>
+                        <section className="card__body row contact__card">
+                            <div className="card__text contact__card">
+                                <form>
+                                    <label htmlFor="name">Name</label>
+                                    <br></br>
+                                    <input id="name" type="text" />
+                                    <br></br>
+                                    <label htmlFor="email">Email</label>
+                                    <br></br>
+                                    <input id="email" type="text" />
+                                    <br></br>
+                                    <label htmlFor="message">Message</label>
+                                    <br></br>
+                                    <input id="message" type="text" />
+                                    <br></br>
+                                    <input id="submit" type="button" value="SUBMIT" onClick={handleSubmit}/>
+                                </form>
                             </div>
                         </section>
                     </article>
