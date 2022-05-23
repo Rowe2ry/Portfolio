84908dccfdbac82943ebf75762f2c32052d4d936
Chris Rowe <rowe2ry@yahoo.com>
2022-05-23 16:51:45 -0400

    added logic to mak contact form email me upon submission

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,31 @@
+import React, { useState } from 'react';
+
 const Contact = () => {
+    const [name,setName] = useState();
+    const [email,setEmail] = useState();
+    const [message,setMessage] = useState();
+
+    const handleInput = (e) => {
+        const { name, value } = e.target;
+
+        if ( name === 'name') {
+            return setName(value);
+        } else if ( name === 'email') {
+            return setEmail(value);
+        } else {
+            return setMessage(value)
+        }
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        
-    }
+
+        window.location.href = "mailto: rowe2ry@gmail.com"+'?cc='+email+'&subject=Portfolio Response'+'&body='+message;
+
+        setName('');
+        setEmail('');
+        setMessage('')
+    };
 
     return (
         <div className="background">
@@ -18,15 +40,33 @@ const Contact = () => {
                                 <form>
                                     <label htmlFor="name">Name</label>
                                     <br></br>
-                                    <input id="name" type="text" />
+                                    <input 
+                                        name="name"
+                                        id="name"
+                                        type="text"
+                                        value ={name}
+                                        onChange={handleInput}
+                                    />
                                     <br></br>
                                     <label htmlFor="email">Email</label>
                                     <br></br>
-                                    <input id="email" type="text" />
+                                    <input 
+                                        name="email"
+                                        id="email"
+                                        type="text"
+                                        value ={email}
+                                        onChange={handleInput}
+                                    />
                                     <br></br>
                                     <label htmlFor="message">Message</label>
                                     <br></br>
-                                    <input id="message" type="text" />
+                                    <textarea 
+                                        name="message"
+                                        id="message"
+                                        type="text"
+                                        value ={message}
+                                        onChange={handleInput}
+                                    />
                                     <br></br>
                                     <input id="submit" type="button" value="SUBMIT" onClick={handleSubmit}/>
                                 </form>
