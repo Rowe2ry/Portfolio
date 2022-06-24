50ae8480ff76a1e499217fc80c7ec514be03b849
Chris Rowe <rowe2ry@yahoo.com>
2022-06-23 23:04:20 -0400

    Removed personal details for about me

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -22,18 +22,6 @@ const AboutMe = () => {
                         </section>
                     </article>
                 </div>
-                <div className="row">
-                    <article className="card">
-                        <section className="card__header">
-                            <h2 className="card__title">Who I am</h2>
-                        </section>
-                        <section className="card__body row">
-                            <div className="card__text">
-                                <p>I'm a {new Date().getFullYear() - 1990} year old living in Atlanta, GA. My pronouns are "Him/He" and currently have 1 child. My wife, Merry and I care about family, community, and creating environments where people are constantly able to grow. We have 2 cats and we host international language students out of our home.</p>
-                            </div>
-                        </section>
-                    </article>
-                </div>
             </div>
         </div>
     );
